0a69a54c2857f670ec47d698b4698cf250032286
Nodeions <59334371+Nodeions@users.noreply.github.com>
2024-04-08 13:18:59 +0200

    08_04_24
    Nettoyage du code, class Deck / Card / Shop

--- Card.py
+++ Card.py
@@ -1,117 +1,98 @@
 # from PIL import Image
 import pygame as py
 #from main import *
+import random
 
-isBoughtCard = False
-isBoughtUpgrade = False
-isActivable = False
 
-flower = 0
-cardLevel = 0
-maxUpgradeLevel = 3
 
 backgroundShop = py.image.load("Ressources/signHangingBed").convert()
 card = py.image.load("Ressources/windowOpen").convert()
 
 
-class Deck:
+class Inventory:
 
     def __init__(self):
-        self.cardList = ["carte pétée sa mère", "test", "wolaaaaa"]
+        self.cardList = []
+        flower = 100
     
 
 class Card:
 
-    def __init__(self):
-        self.isAvailable = False 
-        pass
+    MAX_UPGRADE_LEVEL = 3
 
-    # def update(self):
-    #     self.player.update()
-    #     for elt in self.ground:
-    #         elt.update()
-    #     self.camera.update()
+    def __init__(self):
+        self.title = "Default Title"
+        self.description = "Default Description"
+        self.upgradeDescription = "Defaut Upgrade Description"
+        self.cooldown = 2
+        self.cost = 2
+        self.upgradeCost = 3
+        self.isBuyable = True 
+        self.isBoughtUpgrade = False
+        cardLevel = 0
+
+        pass
 
     #Achetée ou non (donc activée dans l'inventaire ou non)
     #appelé dans l'inventaire
-    def isBought(self):
-        for i in range(len(cardList)-1):
-            if (isBoughtCard == True):
-                isActivable = True
-                availableCardList[i] = cardList[i]
-                cardList[i].remove()
+    def buyCard(self):
+        for i in range(len(Inventory.cardList)-1):
+            if(Inventory.flower >= self.cost):
+                if (self.isBuyable == True):
+                    self.isBuyable = False
+                    Inventory.cardList[i].append()
+                    Inventory.flower -= self.cost
+                    print("Merci pour votre achat de carte !")
+                else:
+                    print("Vous possédez déjà cette carte")
             else:
-                isActivable = False
+                print("Vous n'avez pas assez de fleurs !")
 
     #Upgrade *3 maxi achetable contre de l'argent
 
     def UpgradeCard(self):
-        if (isActivable == True and cardLevel < maxUpgradeLevel and isBoughtUpgrade == True):
-            cardLevel += 1
+        isBoughtUpgrade = input("Voulez-vous acheter l'amélioration ?")
+        if (isBoughtUpgrade.lower() == 'y'):
+            if (cardLevel < self.MAX_UPGRADE_LEVEL):
+                cardLevel += 1
+                Inventory.flower -= self.upgradeCost
+            else:
+                print("Vous êtes déjà au maximum !")
+        else:
+            print("Okay pas d'upgrade dans ce cas !")
 
     #affichage carte
-    def draw(self):
-        Image.open(houseDarkMidRight) 
-
-
-
-
-
-#class DashCard
-
-    #Mettre un cooldown
-    #backgroundShop = py.image.load("Ressources/DashCard").convert()
-    #def CardCooldown():
 
 
 class Shop:
 
     def __init__(self):
+        cardPlacement = []
         pass
-
-    def draw(self):
-        Image.open(signHangingBed) 
-
-    # def update(self):
-        #self.player.update()
-        #for elt in self.ground:
-        #elt.update()
-        #self.camera.update()
-        #Achat de carte --> Check si le joueur a assez de fleurs 
-        #et pas de doublon de carte dans l'inventaire ET dans la boutique (index unique)
-
-    def isEnoughFlower(self):
-        if (flower < 0):
-            print("Voici les cartes : " + Deck.cardList[0])
-            randomPrintCard()
-        else:
-            print("Vous n'avez aucune fleur sur vous")
-
-        
 
     #Montrer des cartes de la boutique aléatoirement parmi une sélection
     def randomPrintCard(self):
         while (i < 5):
-            rand = random.randrange(len(cardList)-1)
-            print(cardList[rand])
+            rand = random.randrange(0, len(Inventory.cardList)-1)
+            print(Inventory.cardList[rand])
             i += 1
 
     def quitShop(self):
         #sera utilisé lors de la selection du bouton quit
+        exit(1)
 
 
 class DeckShop():
     def __init__(self):
         self.SlotList = [" "," "," "," "," "]
-
-        for (i < 5):
+        for i in range(5):
             for i in range(len(self.InvContents)):
                 if self.InvContents[i] == 'Dash':
-                    SCREEN.blit(py.image.load("Resources/Godspeed_Soul_Card.webp"), (WIDTH-(200+(100*i)), HEIGHT-250))
+                    py.SCREEN.blit(py.image.load("Resources/Godspeed_Soul_Card.webp"), (py.WIDTH-(200+(100*i)), py.HEIGHT-250))
                 if self.InvContents[i] == 'Jump+':
-                    SCREEN.blit(py.image.load("Resources/Base pack/bg_castle.png"), (WIDTH-(200+(100*i)), HEIGHT-250))
+                    py.SCREEN.blit(py.image.load("Resources/Base pack/bg_castle.png"), (py.WIDTH-(200+(100*i)), py.HEIGHT-250))
                 if self.InvContents[i] == 'Bomb':
-                    SCREEN.blit(py.image.load("Resources/Purify_Soul_Card.webp"), (WIDTH-(200+(100*i)), HEIGHT-250))
+                    py.SCREEN.blit(py.image.load("Resources/Purify_Soul_Card.webp"), (py.WIDTH-(200+(100*i)), py.HEIGHT-250))
 
                 i += 1
 
@@ -119,4 +100,10 @@ class DeckShop():
     #Bulle de dialogue ou description d'object
 
     #Quitter la boutique
+def main():
+    dashCard = Card()
+    print(dashCard.isBuyable)
+    dashCard.isBuyable = False 
+    print(dashCard.isBuyable)
 
+main()
