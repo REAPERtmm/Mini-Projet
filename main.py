36a001936e76ed7007d4a1c76188388503d7ac27
Amelia <147034763+DivineVoices@users.noreply.github.com>
2024-04-08 13:07:39 +0200

    Fleur Menu et Cartes Nul a chier

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ class Game:
         self.camera.update()
 
     def draw(self):
+        pygame.font.init()
+        my_font = pygame.font.SysFont('Resources/GEO_AI__.TTF', 64)
         self.map.blit(SCREEN, self.camera)
 
         if self.tabPressed:
@@ -49,13 +51,23 @@ class Game:
             if self.InvContents[i] == 'Bomb':
                 card_image = py.image.load("Resources/Purify_Soul_Card.webp")
             if card_image is not None:
+                card_x = ((WIDTH) - (card_image.get_width() * i)) - card_image.get_width()
+                card_y = (HEIGHT + 20) - card_image.get_height()
                 card_width = int(card_image.get_width() * card_scale)
                 card_height = int(card_image.get_height() * card_scale)
-                card_x = ((WIDTH - 30) - (168 * i)) - 168
-                card_y = (HEIGHT - 30) - 229
                 card_image = py.transform.scale(card_image, (card_width, card_height))
                 card_rect = card_image.get_rect(topleft=(card_x, card_y))
                 SCREEN.blit(card_image, card_rect)
+
+        ImageB = py.transform.scale(py.image.load("Resources/collectible_fleur.png"), (64, 64))
+
+        tracks = [self.BlueFlow, self.RedFlow, self.WhiteFlow]
+        colors = [(51, 96, 163), (173, 56, 45), (242, 246, 252)]
+        for i, (track, color) in enumerate(zip(tracks, colors)):
+            track_text = my_font.render(str(track), True, color)
+            SCREEN.blit(ImageB, (64, 12 + i * 64))
+            SCREEN.blit(track_text, (24, 24 + i * 64))
+                
 
         py.display.flip()
 
@@ -101,28 +113,22 @@ class Game:
                         self.down = False
                     if event.key == py.K_b:
                         self.BlueFlow += 1
-                        print(self.BlueFlow, self.RedFlow, self.WhiteFlow, self.InvContents, self.CardScale)
                     if event.key == py.K_r:
                         self.RedFlow += 1
-                        print(self.BlueFlow, self.RedFlow, self.WhiteFlow, self.InvContents, self.CardScale)
                     if event.key == py.K_w:
                         self.WhiteFlow += 1
-                        print(self.BlueFlow, self.RedFlow, self.WhiteFlow, self.InvContents, self.CardScale)
                     if event.key == py.K_i:
                         if len(self.InvContents) >= 3:
                             self.InvContents.pop(0)
                         self.InvContents.append("Dash")
-                        print(self.BlueFlow, self.RedFlow, self.WhiteFlow, self.InvContents, self.CardScale)
                     if event.key == py.K_o:
                         if len(self.InvContents) >= 3:
                             self.InvContents.pop(0)
                         self.InvContents.append("Jump+")
-                        print(self.BlueFlow, self.RedFlow, self.WhiteFlow, self.InvContents, self.CardScale)
                     if event.key == py.K_p:
                         if len(self.InvContents) >= 3:
                             self.InvContents.pop(0)
                         self.InvContents.append("Bomb")
-                        print(self.BlueFlow, self.RedFlow, self.WhiteFlow, self.InvContents, self.CardScale)
                     if event.key == py.K_1:
                         selected_card = 2
                     if event.key == py.K_2:
@@ -147,8 +153,8 @@ class Game:
                         else:
                             self.tabPressed = True
 
-            self.CardScale = [1.0, 1.0, 1.0]
-            self.CardScale[selected_card] = 1.1
+            self.CardScale = [0.7, 0.7, 0.7]
+            self.CardScale[selected_card] = 0.9
 
             
 
